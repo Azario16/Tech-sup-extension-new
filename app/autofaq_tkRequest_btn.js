a4c2939626bd3683813cc24ca199b63b9af9da86
Rahid <r.g.guseynov@skyeng.ru>
2021-10-17 02:58:18 +0500

    подсветка кнопки Взять

--- app/autofaq_tkRequest_btn.js
+++ app/autofaq_tkRequest_btn.js
@@ -1,6 +1,6 @@
 setInterval(() => {
     if (window.location.href === "https://skyeng.autofaq.ai/tickets/assigned") {
-        let btn = document.querySelector('button[type="button"][class="ant-btn expert-get_ticket ant-btn-block"]');
+        let btn = document.querySelectorAll('button[type="button"][class="ant-btn expert-get_ticket ant-btn-block"]')[1];
         if (btn) {
             (btn.innerText === 'Взять запрос (0)') ? btn.style.backgroundColor = 'white' : btn.style.backgroundColor = 'red';
         }
